9d3c0a5f75db4233ed49cde35cbb70ada8d240f6
Nikhil-VIP <nikhilchauha007@gmail.com>
2026-03-25 19:59:01 +0530

    .

--- front_end/quest/src/dash.jsx
+++ front_end/quest/src/dash.jsx
@@ -1,8 +1,44 @@
+import Styles from './dash.module.css';
 const Dashboard = () => {
   return (
     <>
-      <h1> we made it</h1>
+      <div className={Styles.navbar}>
+        <input className={Styles.nav_in}></input>
+
+        <div className={Styles.profile}>
+          hi
+        </div>
+
+      </div>
+
+      <div className={Styles.top}>
+
+        <h1>Dashboard</h1>
+
+        <button className={Styles.top_button}>+ New</button>
+
+      </div>
+
+      <div className={Styles.quick_info}>
+
+        <div className={Styles.cont_one}>
+          hi
+        </div>
+
+        <div className={Styles.cont_two}>
+          hi
+        </div>
+
+        <div className={Styles.cont_three}>
+          hi
+        </div>
+
+      </div>
+
+      <div className={Styles.main}>
+        fvdfv
+      </div>
     </>
-  )
-}
+  );
+};
 export default Dashboard;
